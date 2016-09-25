969795ab33d6bd0a58fdb5c878a4c9edefa712ce
Drew Heenan <drewheenan27@gmail.com>
2016-09-25 17:00:38 -0400

    Removed default headers.

--- src/NameLoader.java
+++ src/NameLoader.java
@@ -11,12 +11,6 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Drew Heenan

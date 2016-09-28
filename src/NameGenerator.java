2cbb66ee5a0bddf6b9350e9483ea3dfa2bd6b35d
Drew Heenan <drewheenan27@gmail.com>
2016-09-28 14:11:31 -0400

    Spring cleaning of imports.

--- src/NameGenerator.java
+++ src/NameGenerator.java
@@ -1,15 +1,6 @@
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Map;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
